15c6c2c2ef8970ba906ece19b73a833458634c35
Jan Krueger <jankrueger1999@gmail.com>
2022-07-02 12:44:43 +0200

    chore: add typeorm

--- apps/rooftop-api/src/app/app.module.ts
+++ apps/rooftop-api/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getMetadataArgsStorage } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+        logging: configService.get('DB_LOGGING', false),
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- apps/rooftop-api/src/main.ts
+++ apps/rooftop-api/src/main.ts
@@ -1,18 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Rooftop API')
+    .setDescription('Rooftop API')
+    .setVersion('1.0')
+    .addTag('Users')
+    .addTag('Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
