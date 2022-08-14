059683119954fddf70b348a1d66eb508b4bcbb5e
Jan Krueger <jankrueger1999@gmail.com>
2022-08-14 21:02:05 +0200

    feat(app): add token service

--- apps/farmer-companion/src/app/token.service.spec.ts
+++ apps/farmer-companion/src/app/token.service.spec.ts
@@ -1,6 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TokenService } from './token.service';
+import * as dayjs from 'dayjs';
+import {
+  ACCESS_TOKEN_EXPIRATION_KEY,
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_EXPIRATION_KEY,
+  REFRESH_TOKEN_KEY,
+  SetTokenPair,
+  TokenService,
+} from './token.service';
 
 describe('TokenService', () => {
   let service: TokenService;
@@ -8,9 +16,113 @@ describe('TokenService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TokenService);
+
+    // Clear local storage and session storage.
+    localStorage.clear();
+    sessionStorage.clear();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be able to get a token pair', () => {
+    const refreshTokenValue = 'refreshTokenValue';
+    const refreshTokenExpiration = dayjs().add(90, 'day').unix();
+
+    const accessTokenValue = 'accessTokenValue';
+    const accessTokenExpiration = dayjs().add(5, 'minute').unix();
+
+    localStorage.setItem(REFRESH_TOKEN_KEY, refreshTokenValue);
+    localStorage.setItem(
+      REFRESH_TOKEN_EXPIRATION_KEY,
+      refreshTokenExpiration.toString()
+    );
+
+    sessionStorage.setItem(ACCESS_TOKEN_KEY, accessTokenValue);
+    sessionStorage.setItem(
+      ACCESS_TOKEN_EXPIRATION_KEY,
+      accessTokenExpiration.toString()
+    );
+
+    const tokenPair = service.tokenPair;
+
+    expect(tokenPair.refreshToken).toBe(refreshTokenValue);
+    expect(tokenPair.accessToken).toBe(accessTokenValue);
+
+    expect(tokenPair.refreshTokenExpiration).toStrictEqual(
+      dayjs.unix(refreshTokenExpiration).toDate()
+    );
+    expect(tokenPair.accessTokenExpiration).toStrictEqual(
+      dayjs.unix(accessTokenExpiration).toDate()
+    );
+  });
+
+  it('should be able to save a token pair', () => {
+    const tokenPair: SetTokenPair = {
+      accessToken: 'accessTokenValue',
+      accessTokenExpiration: dayjs().add(5, 'minute').toDate(),
+      refreshToken: 'refreshTokenValue',
+      refreshTokenExpiration: dayjs().add(90, 'day').toDate(),
+    };
+
+    service.saveTokenPair(tokenPair);
+
+    expect(localStorage.getItem(REFRESH_TOKEN_KEY)).toBe(
+      tokenPair.refreshToken
+    );
+
+    expect(localStorage.getItem(REFRESH_TOKEN_EXPIRATION_KEY)).toBe(
+      dayjs(tokenPair.refreshTokenExpiration).unix().toString()
+    );
+
+    expect(sessionStorage.getItem(ACCESS_TOKEN_KEY)).toBe(
+      tokenPair.accessToken
+    );
+
+    expect(sessionStorage.getItem(ACCESS_TOKEN_EXPIRATION_KEY)).toBe(
+      dayjs(tokenPair.accessTokenExpiration).unix().toString()
+    );
+  });
+
+  it('should be able to determine if the stored access token is expired', () => {
+    // Store a expired expiration date
+    assertExpiration(
+      service,
+      'accessTokenExpired',
+      sessionStorage,
+      ACCESS_TOKEN_EXPIRATION_KEY
+    );
+  });
+
+  it('should be able to determine if the stored refresh token is expired', () => {
+    assertExpiration(
+      service,
+      'refreshTokenExpired',
+      localStorage,
+      REFRESH_TOKEN_EXPIRATION_KEY
+    );
+  });
 });
+
+function assertExpiration(
+  service: TokenService,
+  getterFunctionName: keyof TokenService,
+  storage: Storage,
+  key: string
+) {
+  // Assert that the getter returns true when the expiration date is in the past.
+  storage.setItem(key, dayjs().subtract(1, 'day').unix().toString());
+
+  expect(service[getterFunctionName]).toBe(true);
+
+  // Assert that the getter returns false when the expiration date is in the future.
+  storage.setItem(key, dayjs().add(1, 'day').unix().toString());
+
+  expect(service[getterFunctionName]).toBe(false);
+
+  // Assert that the getter returns true when the expiration date is not set.
+  storage.clear();
+
+  expect(service[getterFunctionName]).toBe(true);
+}
--- apps/farmer-companion/src/app/token.service.ts
+++ apps/farmer-companion/src/app/token.service.ts
@@ -1,9 +1,99 @@
 import { Injectable } from '@angular/core';
+import * as dayjs from 'dayjs';
+
+export const ACCESS_TOKEN_KEY = 'rooftop_access_token';
+export const REFRESH_TOKEN_KEY = 'rooftop_refresh_token';
+export const REFRESH_TOKEN_EXPIRATION_KEY = 'rooftop_refresh_token_expiration';
+export const ACCESS_TOKEN_EXPIRATION_KEY = 'rooftop_access_token_expiration';
+
+export type SetTokenPair = {
+  accessToken: string;
+  accessTokenExpiration: Date;
+  refreshToken: string;
+  refreshTokenExpiration: Date;
+};
+
+export type TokenPair = Partial<SetTokenPair>;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
+  constructor() {}
 
-  constructor() { }
+  get tokenPair(): TokenPair {
+    const refreshTokenValue = this.refreshToken;
+    const refreshTokenExpiration = localStorage.getItem(
+      REFRESH_TOKEN_EXPIRATION_KEY
+    );
+    const accessTokenValue = this.accessToken;
+    const accessTokenExpiration = sessionStorage.getItem(
+      ACCESS_TOKEN_EXPIRATION_KEY
+    );
+
+    // Since the expiration is stored as a unix timestamp, we need to convert it to a Date object.
+    const refreshTokenExpirationDate = refreshTokenExpiration
+      ? dayjs.unix(parseInt(refreshTokenExpiration, 10)).toDate()
+      : undefined;
+    const accessTokenExpirationDate = accessTokenExpiration
+      ? dayjs.unix(parseInt(accessTokenExpiration, 10)).toDate()
+      : undefined;
+
+    return {
+      accessToken: accessTokenValue ?? undefined,
+      refreshToken: refreshTokenValue ?? undefined,
+      accessTokenExpiration: accessTokenExpirationDate,
+      refreshTokenExpiration: refreshTokenExpirationDate,
+    };
+  }
+
+  public saveTokenPair(tokenPair: SetTokenPair): void {
+    localStorage.setItem(REFRESH_TOKEN_KEY, tokenPair.refreshToken);
+    localStorage.setItem(
+      REFRESH_TOKEN_EXPIRATION_KEY,
+      dayjs(tokenPair.refreshTokenExpiration).unix().toString()
+    );
+
+    sessionStorage.setItem(ACCESS_TOKEN_KEY, tokenPair.accessToken);
+    sessionStorage.setItem(
+      ACCESS_TOKEN_EXPIRATION_KEY,
+      dayjs(tokenPair.accessTokenExpiration).unix().toString()
+    );
+  }
+
+  get accessToken(): string | undefined {
+    return sessionStorage.getItem(ACCESS_TOKEN_KEY) ?? undefined;
+  }
+
+  get refreshToken(): string | undefined {
+    return localStorage.getItem(REFRESH_TOKEN_KEY) ?? undefined;
+  }
+
+  get accessTokenExpired(): boolean {
+    const accessTokenExpiration = sessionStorage.getItem(
+      ACCESS_TOKEN_EXPIRATION_KEY
+    );
+    if (accessTokenExpiration === null) {
+      return true;
+    }
+
+    const accessTokenExpirationDate = dayjs.unix(
+      parseInt(accessTokenExpiration, 10)
+    );
+    return accessTokenExpirationDate.isBefore(dayjs());
+  }
+
+  get refreshTokenExpired(): boolean {
+    const refreshTokenExpiration = localStorage.getItem(
+      REFRESH_TOKEN_EXPIRATION_KEY
+    );
+    if (refreshTokenExpiration === null) {
+      return true;
+    }
+
+    const refreshTokenExpirationDate = dayjs.unix(
+      parseInt(refreshTokenExpiration, 10)
+    );
+    return refreshTokenExpirationDate.isBefore(dayjs());
+  }
 }
