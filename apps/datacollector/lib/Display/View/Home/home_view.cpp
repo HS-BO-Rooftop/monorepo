a465581fc0943f41fe0438d5dcb04787cb2566ea
Martin Peth <martin@peth.pl>
2022-09-26 03:43:02 +0200

    update: view templates

---
+++ apps/datacollector/lib/Display/View/Home/home_view.cpp
@@ -0,0 +1,1 @@
+#include "home_view.h"
---
+++ apps/datacollector/lib/Display/View/Home/home_view.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Arduino.h>
+#include "../view.h"
+
+class HomeView : public View
+{
+    public:
+        int render();
+    protected:
+    private:
+};
