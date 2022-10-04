f5ac0d750eec3e8f421d39f99329e73db62550ec
Jan Krueger <jankrueger1999@gmail.com>
2022-10-04 12:31:28 +0200

    feat(app): add android support

--- apps/farmer-companion/capacitor.config.ts
+++ apps/farmer-companion/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'farmer-companion',
+  appId: 'de.hs-bochum.ontop.farmer-companion',
+  appName: 'Ontop Farmer Companion',
   webDir: '../../dist/apps/farmer-companion',
   bundledWebRuntime: false,
 };
---
+++ apps/farmer-companion/src/environments/environment.android-dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://10.0.2.2:3000/api',
+};
